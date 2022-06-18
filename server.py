e093198b26de0ab8759316bd2372d667f1bf077c
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-06-18 07:59:27 -0400

    resolution changes

--- server.py
+++ server.py
@@ -271,7 +271,8 @@ def dai_rgb_pose():
   xoutRgb.setStreamName("rgb")
 
   # Properties
-  camRgb.setPreviewSize(300, 300)
+  camRgb.setPreviewSize(4208, 3120) # full resolution
+  #camRgb.setPreviewSize(4208 // 4, 3120 // 4) # quarter resolution
   camRgb.setInterleaved(False)
   camRgb.setColorOrder(depthai.ColorCameraProperties.ColorOrder.RGB)
 
